83e8ac265ee238445e3c969987394cf08de47261
nikhilnitrr <nikhilchandrakar3@gmail.com>
2021-09-24 18:48:58 +0530

    validated amounts

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 var billref=document.querySelector('#bill-amount');
 var cashref=document.querySelector('#cash-amount');
 var btnref=document.querySelector('#btn-primary');
+var error=document.querySelector('#error-message');
 
 
-function myScript(){
-var billAmount=billref.value;
-var cashAmount=cashref.value;
+function myScript()
+{
+var billAmount=parseInt(billref.value);
+var cashAmount=parseInt(cashref.value);
 
-console.log(billAmount);
-console.log(cashAmount);
+    if(billAmount<0 || cashAmount<0)
+    {
+        error.innerText="Invalid input";
+    }
+
+    else if(cashAmount<billAmount)
+    {
+        error.innerText="Do you want to get beaten?";
+    }
 }
 
 
